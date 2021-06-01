c62c9fbdf7583566dd0ff54386fcd119b26a4f75
Chris Palmer <chris.palmer@fusionmetric.com>
2021-06-01 17:40:49 -0500

    Created Xamarin Forms projects

---
+++ ARProject/ARProject-Mobile/ARProject-Mobile/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ARProject_Mobile.ViewModels;
+using ARProject_Mobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ARProject_Mobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ARProject/ARProject-Mobile/ARProject-Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ARProject_Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ARProject_Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
