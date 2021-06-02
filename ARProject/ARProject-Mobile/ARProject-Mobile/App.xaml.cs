885287824edb57b432ab393e4ae81219c1806105
Chris Palmer <chris.palmer@fusionmetric.com>
2021-06-02 13:30:04 -0500

    Adding Android keys for MS App Center

--- ARProject/ARProject-Mobile/ARProject-Mobile/App.xaml.cs
+++ ARProject/ARProject-Mobile/ARProject-Mobile/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using ARProject_Mobile.Services;
 using ARProject_Mobile.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -19,6 +22,10 @@ namespace ARProject_Mobile
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=f276defd-1aae-4fda-87c6-2e0b27397132;" +
+                "uwp={Your UWP App secret here};" +
+                "ios={Your iOS App secret here}",
+                typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
