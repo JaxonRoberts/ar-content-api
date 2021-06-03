7053b0001a8142a13e5ff9f484510b4cf631e23a
Chris Palmer <chris.palmer@fusionmetric.com>
2021-06-03 11:57:00 -0500

    FileUploadController constructor and TODO for Jaxon

--- ARProject/ARProject-API/Controllers/FileUploadController.cs
+++ ARProject/ARProject-API/Controllers/FileUploadController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ARProject_API.Controllers
 {
@@ -11,9 +13,21 @@ namespace ARProject_API.Controllers
     [ApiController]
     public class FileUploadController : ControllerBase
     {
-        public FileUploadController()
+        private readonly ILogger<FileUploadController> _logger;
+        private readonly IWebHostEnvironment _environment;
+        public FileUploadController(ILogger<FileUploadController> logger,
+            IWebHostEnvironment environment)
         {
+            _logger = logger;
+            _environment = environment ?? throw new ArgumentException(nameof(Environment));
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            // TODO
+
+            return Ok();
         }
 
     }
