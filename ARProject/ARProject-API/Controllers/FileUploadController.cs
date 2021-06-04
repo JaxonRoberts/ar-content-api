432838b5f0fda0e5115ff710d0d95d1427c6bfd6
Jaxon Roberts <jaxonr1217@gmail.com>
2021-06-04 15:19:05 -0500

    Updated Post method

--- ARProject/ARProject-API/Controllers/FileUploadController.cs
+++ ARProject/ARProject-API/Controllers/FileUploadController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -25,9 +26,38 @@ namespace ARProject_API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post()
         {
-            // TODO
+            try
+            {
+                var httpRequest = HttpContext.Request;
 
-            return Ok();
+                if (httpRequest.Form.Files.Count > 0)
+                {
+                    foreach (var file in httpRequest.Form.Files)
+                    {
+                        var filePath = Path.Combine(_environment.ContentRootPath, "uploads");
+
+                        if (!Directory.Exists(filePath))
+                            Directory.CreateDirectory(filePath);
+
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await file.CopyToAsync(memoryStream);
+                            System.IO.File.WriteAllBytes(Path.Combine(filePath,
+                                file.FileName), memoryStream.ToArray());
+                        }
+
+                        return Ok();
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error");
+                return new StatusCodeResult(500);
+            }
+
+            return BadRequest();
         }
 
         [HttpGet]
