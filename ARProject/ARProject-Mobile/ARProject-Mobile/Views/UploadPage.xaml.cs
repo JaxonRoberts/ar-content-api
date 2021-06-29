4618ad9b4fb9c9ce010f0b49c2539475817a4480
Jaxon Roberts <jaxonjroberts@gmail.com>
2021-06-29 12:03:34 -0500

    added the Button_Clicked method

--- ARProject/ARProject-Mobile/ARProject-Mobile/Views/UploadPage.xaml.cs
+++ ARProject/ARProject-Mobile/ARProject-Mobile/Views/UploadPage.xaml.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,22 @@ namespace ARProject_Mobile.Views
         public UploadPage()
         {
             InitializeComponent();
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var file = await MediaPicker.PickPhotoAsync();
+
+            if (file == null)
+                return;
+
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(await file.OpenReadAsync()), "file", file.FileName);
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.PostAsync("", content);
+
+            StatusLabel.Text = response.StatusCode.ToString();
         }
     }
 }
